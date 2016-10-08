e769c7c0038e62201f88af927d4173528905390c
Sage Price <sageeprice@gmail.com>
2016-10-08 11:46:59 -0700

    Solved 9 and 10, updated spacing in answers.txt

--- src/problems/Runner.java
+++ src/problems/Runner.java
@@ -1,6 +1,6 @@
 package problems;
 
-import problems.euler001to025.Euler008;
+import problems.euler001to025.Euler010;
 
 /**
  * Created by Sage on 10/7/16.
@@ -8,6 +8,6 @@ import problems.euler001to025.Euler008;
 public class Runner {
 
     public static void main(String[] args) {
-        System.out.println(new Euler008().solve());
+        System.out.println(new Euler010().solve());
     }
 }
---
+++ src/problems/euler001to025/Euler010.java
@@ -0,0 +1,44 @@
+package problems.euler001to025;
+
+import problems.Problem;
+
+/**
+ * Problem 10:
+ * Find the sum of all the primes below two million.
+ *
+ * Solved by Sage on 10/8/16.
+ */
+public class Euler010 implements Problem {
+
+    private static final int SIEVE_SIZE = 2000000;
+
+    /**
+     * Same as problem 3, just use the sieve.
+     * Will extract this to a library at some point,
+     * sieve is a very common pattern.
+     */
+    @Override
+    public String solve() {
+
+        // Simple sieve implementation
+        // Magic number 10000 calculated retrospectively
+        boolean[] isPrime = new boolean[SIEVE_SIZE + 1];
+
+        long primeSum = 0;
+
+        for (int i = 2; i <= SIEVE_SIZE; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i <= SIEVE_SIZE; i++) {
+            if (isPrime[i]) {
+                for (int j = 2; j * i <= SIEVE_SIZE; j++) {
+                    isPrime[j * i] = false;
+                }
+                primeSum += i;
+            }
+        }
+
+        return String.valueOf(primeSum);
+    }
+}
