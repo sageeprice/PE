fd68fffa673d432183a4912db23c36154a22b841
Sage Price <sageeprice@gmail.com>
2016-10-14 21:13:15 -0700

    17,18,19

--- src/problems/Runner.java
+++ src/problems/Runner.java
@@ -1,6 +1,6 @@
 package problems;
 
-import problems.euler001to025.Euler016;
+import problems.euler001to025.Euler019;
 
 /**
  * Created by Sage on 10/7/16.
@@ -9,7 +9,7 @@ public class Runner {
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        System.out.println(new Euler016().solve());
-        System.out.println("Time to compute was " + (System.currentTimeMillis()-startTime));
+        System.out.println(new Euler019().solve());
+        System.out.println("Time to compute was " + (System.currentTimeMillis()-startTime) + " milliseconds.");
     }
 }
