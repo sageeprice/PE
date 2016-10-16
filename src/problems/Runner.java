b5eae1c3eb3dfe2193e7a2afa1aa5e9eb758776d
Sage Price <sageeprice@gmail.com>
2016-10-16 13:20:05 -0700

    26

--- src/problems/Runner.java
+++ src/problems/Runner.java
@@ -1,6 +1,6 @@
 package problems;
 
-import problems.euler001to025.Euler025;
+import problems.euler025to050.Euler026;
 
 /**
  * Created by Sage on 10/7/16.
@@ -9,7 +9,7 @@ public class Runner {
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        System.out.println(new Euler025().solve());
+        System.out.println(new Euler026().solve());
         System.out.println("Time to compute was " + (System.currentTimeMillis()-startTime) + " milliseconds.");
     }
 }
---
+++ src/problems/euler025to050/Euler026.java
@@ -0,0 +1,56 @@
+package problems.euler025to050;
+
+import problems.Problem;
+
+import java.math.BigInteger;
+
+/**
+ * Created by Sage on 10/16/16.
+ */
+public class Euler026 implements Problem {
+
+    @Override
+    public String solve() {
+
+        for (int i = 999; i > 10; i -= 2) {
+            // period is always a factor of denominator - 1,
+            // best possible is for primes where period is p - 1
+            // so we just find the first prime less than 1000
+            // where the period is p - 1
+            if (isPrime(i)) {
+                // best possible period is one less than the denominator
+                if (findPeriod(i) == i - 1) {
+                    return String.valueOf(i);
+                }
+            }
+        }
+        return null;
+    }
+
+    private boolean isPrime(int x) {
+
+        for (int i = 2; i <= Math.sqrt(x); i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Calculates the period of decimal representation of 1/x
+     * by calculating multiplicative order of 10 base x
+     * @param x the base
+     * @return multiplicative order of 10 base x
+     */
+    private int findPeriod(int x) {
+        BigInteger tenPow = BigInteger.TEN;
+        int pow = 1;
+        BigInteger bigX = new BigInteger(String.valueOf(x));
+        while (tenPow.remainder(bigX).compareTo(BigInteger.ONE) != 0) {
+            tenPow = tenPow.multiply(BigInteger.TEN);
+            pow++;
+        }
+        return pow;
+    }
+}
