1e470008e1a0b830391e9a19918b77cafa0c36f5
Sage Price <sageeprice@gmail.com>
2016-10-22 21:42:42 -0700

    33

--- src/problems/Runner.java
+++ src/problems/Runner.java
@@ -1,6 +1,6 @@
 package problems;
 
-import problems.euler025to050.Euler032;
+import problems.euler025to050.Euler033;
 
 /**
  * Created by Sage on 10/7/16.
@@ -9,7 +9,7 @@ public class Runner {
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        System.out.println(new Euler032().solve());
+        System.out.println(new Euler033().solve());
         System.out.println("Time to compute was " + (System.currentTimeMillis()-startTime) + " milliseconds.");
     }
 }
---
+++ src/problems/euler025to050/Euler033.java
@@ -0,0 +1,36 @@
+package problems.euler025to050;
+
+import problems.Problem;
+
+/**
+ * Problem 33:
+ * https://projecteuler.net/problem=33
+ *
+ * Solved by Sage on 10/22/16.
+ */
+public class Euler033 implements Problem {
+
+    @Override
+    public String solve() {
+
+        int num = 1;
+        int den = 1;
+
+        for (int denominator = 12; denominator <= 99; denominator++) {
+            for (int numerator = 12; numerator < denominator; numerator++) {
+
+                if (numerator%10 == denominator/10 && numerator/10 != numerator%10) {
+                    if (numerator * (denominator % 10) == denominator * (numerator / 10)) {
+                        num *= numerator / 10;
+                        den *= denominator % 10;
+                    }
+                }
+            }
+        }
+
+        // Worth noting that this won't always necessarily yield the correct value,
+        // but we get a bit lucky here since it simplifies to 1 / 100 rather than
+        // like 3 / 56 or something
+        return String.valueOf(den / num);
+    }
+}
