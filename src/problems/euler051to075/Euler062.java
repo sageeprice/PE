4c39dabe14c1da1b9379f9c3015d39029943bfa7
Sage Price <sageeprice@gmail.com>
2016-10-29 13:33:51 -0700

    63 and 64

---
+++ src/problems/euler051to075/Euler062.java
@@ -0,0 +1,47 @@
+package problems.euler051to075;
+
+import javafx.util.Pair;
+import problems.Problem;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Problem 62:
+ * https://projecteuler.net/problem=62
+ *
+ * Solved by Sage on 10/29/16.
+ */
+public class Euler062 implements Problem {
+
+    private static final int TARGET_PERMUTATIONS = 5;
+
+    @Override
+    public String solve() {
+        // Map of sorted integer string to Pair of count, smallest value in permutation
+        Map<String, Pair<Integer, Long>> permutationCountMap = new HashMap<>();
+
+        for (long i = 1; ; i++) {
+            // Generate cube and get sorted string of it
+            long x = i * i * i;
+            char[] xChars = String.valueOf(x).toCharArray();
+            Arrays.sort(xChars);
+            String orderedXStr = String.valueOf(xChars);
+
+            // Check map and update or return
+            if (permutationCountMap.containsKey(orderedXStr)) {
+                if (permutationCountMap.get(orderedXStr).getKey() == TARGET_PERMUTATIONS - 1) {
+                    return String.valueOf(permutationCountMap.get(orderedXStr).getValue());
+                }
+                permutationCountMap.put(
+                        orderedXStr,
+                        new Pair<>(permutationCountMap.get(orderedXStr).getKey() + 1,
+                                permutationCountMap.get(orderedXStr).getValue()));
+            } else {
+                permutationCountMap.put(orderedXStr,
+                        new Pair<>(1, x));
+            }
+        }
+    }
+}
---
+++ src/problems/euler051to075/Euler063.java
@@ -0,0 +1,30 @@
+package problems.euler051to075;
+
+import problems.Problem;
+
+/**
+ * Problem 63:
+ * https://projecteuler.net/problem=63
+ *
+ * Solved by Sage on 10/29/16.
+ */
+public class Euler063 implements Problem {
+
+    @Override
+    public String solve() {
+
+        // Definitely one of those problems that could easily be done by hand
+
+        int total = 1;
+        for (int i = 2; i < 10; i++) {
+            double x = i;
+            int count = 0;
+            while (Math.log10(x) > count) {
+                x *= i;
+                count++;
+            }
+            total += count;
+        }
+        return String.valueOf(total);
+    }
+}
