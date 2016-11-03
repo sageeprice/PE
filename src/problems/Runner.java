9ca1f0d1433d35a3e6256d1f0b99681b34085f3c
Sage Price <sageeprice@gmail.com>
2016-11-02 22:53:27 -0700

    80

--- src/problems/Runner.java
+++ src/problems/Runner.java
@@ -1,6 +1,6 @@
 package problems;
 
-import problems.euler076To100.Euler079;
+import problems.euler076To100.Euler080;
 
 /**
  * Runner class to execute Project Euler solutions.
@@ -9,7 +9,7 @@ public class Runner {
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        System.out.println(new Euler079().solve());
+        System.out.println(new Euler080().solve());
         System.out.println("Time to compute was " + (System.currentTimeMillis()-startTime) + " milliseconds.");
     }
 }
---
+++ src/problems/euler076To100/Euler080.java
@@ -0,0 +1,52 @@
+package problems.euler076To100;
+
+import problems.Problem;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Problem 80:
+ * https://projecteuler.net/problem=80
+ *
+ * Solved by Sage on 11/1/16.
+ */
+public class Euler080 implements Problem {
+
+    private static final BigDecimal TWO = new BigDecimal(2);
+
+    @Override
+    public String solve() {
+
+        int sum = 0;
+        Set<Integer> squares = new HashSet<>();
+        for (int i = 1; i <= 10; i++) {
+            squares.add(i * i);
+        }
+
+        for (int x = 2; x < 100; x++) {
+            if (squares.contains(x))
+                continue;
+            BigDecimal guess = new BigDecimal(x / 2);
+            BigDecimal square = new BigDecimal(x);
+            for (int i = 2; i < 20; i++) {
+                guess = newtonSqrt(guess, square);
+            }
+            System.out.println(guess);
+            String num = guess.toPlainString().replace(".", "").substring(0,100);
+            for (char c : num.toCharArray()) {
+                sum += c - '0';
+            }
+        }
+        return String.valueOf(sum);
+    }
+
+    // Iterative approximation of square root using Newton's method
+    //
+    // https://en.wikipedia.org/wiki/Newton%27s_method#Square_root_of_a_number
+    private BigDecimal newtonSqrt(BigDecimal x, BigDecimal sq) {
+        return x.subtract(x.multiply(x).subtract(sq).divide(TWO.multiply(x), 150, BigDecimal.ROUND_DOWN));
+    }
+
+}
