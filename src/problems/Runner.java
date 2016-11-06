b5dfbeed669502789b3249c024e886e83fd15778
Sage Price <sageeprice@gmail.com>
2016-11-06 15:07:44 -0800

    113, and cleaned up format of answers.txt

--- src/problems/Runner.java
+++ src/problems/Runner.java
@@ -1,6 +1,6 @@
 package problems;
 
-import problems.euler076To100.Euler098;
+import problems.euler101To125.Euler113;
 
 /**
  * Runner class to execute Project Euler solutions.
@@ -9,7 +9,7 @@ public class Runner {
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        System.out.println(new Euler098().solve());
+        System.out.println(new Euler113().solve());
         System.out.println("Time to compute was " + (System.currentTimeMillis()-startTime) + " milliseconds.");
     }
 }
---
+++ src/problems/euler101To125/Euler113.java
@@ -0,0 +1,57 @@
+package problems.euler101To125;
+
+import problems.Problem;
+
+/**
+ * Problem 113:
+ * https://projecteuler.net/problem=113
+ * Created by Sage on 11/6/16.
+ */
+public class Euler113 implements Problem {
+
+    private static final long DIGITS = 100;
+
+    @Override
+    public String solve() {
+
+        /**
+         * This one came out super short, so here's an overly brief explanation of
+         * how I've been viewing this problem.
+         *
+         * So we are arranging n empty spaces, and k dividers. Position relative to
+         * the dividers determine what the number is. Consider the increasing example:
+         *      _||||_||_||| => 046
+         *       1234 56 789
+         *
+         * The same logic holds for the decreasing example, with an extra divider due
+         * to the fact that you may have leading zeros.
+         *
+         *      ||_|||_||_|||_ => 8530
+         *      09 876 54 321
+         *
+         * So at this point we have two simple combinatorial values to solve, then
+         * we just subtract the shared values (e.g. 1111, 333, 44444) and zeros. The
+         * number of shared values is just 10 times the number of digits.
+         *
+         * ALTERNATIVE: if this doesn't make sense, you can use a dynamic programming
+         * approach, keeping track of the number of increasing/decreasing numbers ending
+         * with each digit and iterating on that. This is however O(n) in the number of
+         * digits, whereas the direct calculation is O(1). Though either should be fine
+         * for any reasonable number of digits.
+         */
+
+        long increasing = nCk(DIGITS + 9, 9);
+        long decreasing = nCk(DIGITS + 10, 10);
+
+        return String.valueOf(increasing + decreasing - 10 * DIGITS - 2);
+    }
+
+    private long nCk(long n, long k) {
+        long prod = 1;
+        for (long i = 1; i <= k; i++) {
+            prod *= (n + 1) - i;
+            prod /= i;
+        }
+        return prod;
+    }
+}
