34f0b8880cdee83f32a74058f9c87e444d548e5b
Sage Price <sageeprice@gmail.com>
2016-11-07 22:19:56 -0800

    108

--- src/problems/Runner.java
+++ src/problems/Runner.java
@@ -1,6 +1,6 @@
 package problems;
 
-import problems.euler101To125.Euler113;
+import problems.euler101To125.Euler108;
 
 /**
  * Runner class to execute Project Euler solutions.
@@ -9,7 +9,7 @@ public class Runner {
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        System.out.println(new Euler113().solve());
+        System.out.println(new Euler108().solve());
         System.out.println("Time to compute was " + (System.currentTimeMillis()-startTime) + " milliseconds.");
     }
 }
