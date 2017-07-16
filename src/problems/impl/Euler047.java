28c33ee41377a2aa0ddef1258d143ac199caacc7
sageeprice <sageeprice@gmail.com>
2017-07-16 17:48:13 -0400

    109

--- src/problems/impl/Euler047.java
+++ src/problems/impl/Euler047.java
@@ -10,26 +10,28 @@ import problems.Problem;
  */
 public class Euler047 implements Problem {
 
+    private static final int LIMIT = 1_000_000;
+
     @Override
     public String solve() {
 
         // An array counting the number of unique prime factors each index has
-        // Same idea as Sieve of Erastosthenes
-        int[] factorSieve = new int[1000000];
+        // Same idea as Sieve of Eratosthenes
+        int[] factorSieve = new int[LIMIT];
 
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < LIMIT; i++) {
             factorSieve[i] = 0;
         }
 
-        for (int i = 2; i < 1000000; i++) {
+        for (int i = 2; i < LIMIT; i++) {
             if (factorSieve[i] == 0) {
-                for (int j = 2*i; j < 1000000; j += i) {
+                for (int j = 2*i; j < LIMIT; j += i) {
                     factorSieve[j]++;
                 }
             }
         }
 
-        for (int i = 2; i < 1000000; i++) {
+        for (int i = 2; i < LIMIT; i++) {
             if (factorSieve[i] == 4
                     && factorSieve[i+1] == 4
                     && factorSieve[i+2] == 4
--- src/problems/impl/Euler109.java
+++ src/problems/impl/Euler109.java
@@ -2,88 +2,149 @@ package problems.impl;
 
 import problems.Problem;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
- * Created by Sage on 11/15/16.
+ * Generate possibilities, eliminate duplicates.
  */
 public class Euler109 implements Problem {
-
-    private static final int ROLLS = 3;
-    private final int LIMIT = 100;
-    private final int POINT_LIMIT = 20;
-    private static final int MAX_MULTIPLIER = 3;
 
     @Override
     public String solve() {
 
-        int count = 0;
-        int sixCount = 0;
-        int[] noRolls = new int[LIMIT];
+        List<Point> hits = new ArrayList<>();
 
-        // first roll
-        int[][] oneRoll = new int[MAX_MULTIPLIER][LIMIT];
-        for (int j = 1; j < POINT_LIMIT; j++) {
-            for (int m = 1; m <= MAX_MULTIPLIER; m++) {
-                oneRoll[m-1][j * m] += 1;
-                if (m == 2) {
-                    count++;
-                    if (m*j == 6) {
-                        System.out.println("6 = " + m + "*" + j);
-                        sixCount++;
-                    }
-                }
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 20; j++) {
+                hits.add(new Point(j, i));
             }
         }
+        hits.add(new Point(25, 1));
+        hits.add(new Point(25, 2));
+        hits.add(new Point(0, 0));
 
-        for (int[] row : oneRoll) {
-            System.out.println(Arrays.toString(row));
-        }
-
-        // second roll
-        int[] twoRolls = new int[LIMIT];
-        for (int j = 1; j < POINT_LIMIT; j++) {
-            for (int m = 1; m <= MAX_MULTIPLIER; m++) {
-                for (int r = 0; r < MAX_MULTIPLIER; r++) {
-                    if (m + r == 3 && r + 1 > m)
+        Set<Game> checkoutGames = new HashSet<>();
+        for (Point hitOne : hits) {
+            if (hitOne.getValue() == 0) {
+                continue;
+            }
+            for (Point hitTwo : hits) {
+                for (Point hitThree : hits) {
+                    if (hitTwo.getValue() == 0 && hitThree.getValue() != 0) {
                         continue;
-                    for (int i = 1; i < LIMIT; i++) {
-                        if (i + j * m >= LIMIT) {
-                            break;
+                    }
+                    Game game = new Game(hitOne, hitTwo, hitThree);
+                    if (game.getScore() < 100 && game.isCheckout()) {
+                        // Games where the order of non-final tosses are inverted
+                        // are thought of as duplicates, should be ignored.
+                        Game flipGame = new Game(hitTwo, hitOne, hitThree);
+                        if (checkoutGames.contains(flipGame) && game.getThird().getValue() != 0) {
+                            continue;
                         }
-                        if (m == 2) {
-                            if (m * j + i == 6) {
-                                System.out.println(r+1 + ", " + m);
-                                System.out.println("6 = " + m + "*" + j + "+" + i + " => " + oneRoll[r][i]);
-                                sixCount += oneRoll[r][i];
-                            }
-                            count += oneRoll[r][i];
-                        }
-                        twoRolls[i + j * m] += oneRoll[r][i];
+                        checkoutGames.add(game);
                     }
                 }
             }
         }
 
-        System.out.println(2 + ": " + Arrays.toString(twoRolls));
+        return String.valueOf(checkoutGames.size());
+    }
 
-        System.out.println(6 + ": " + sixCount);
-        // third roll
-        for (int j = 1; j < POINT_LIMIT; j++) {
-            for (int i = 2; i < LIMIT; i++) {
-                if (i + j * 2 >= LIMIT)
-                    break;
-                if (2 * j + i < LIMIT) {
-                    count += twoRolls[i];
-                    if (2*j+i == 6) {
-                        System.out.println("6 = " + 2 + "*" + j + "+" + i + " => " + twoRolls[i]);
-                        sixCount += twoRolls[i];
-                    }
-                }
-            }
+    private class Point {
+
+        private int section;
+        private int multiplier;
+
+        public Point(int section, int multiplier) {
+           this.section = section;
+           this.multiplier = multiplier;
         }
 
-        System.out.println(6 + ": " + sixCount);
-        return String.valueOf(count);
+        public int getSection() {
+            return section;
+        }
+
+        public int getMultiplier() {
+            return multiplier;
+        }
+
+        public int getValue() {
+            return section * multiplier;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Point)) return false;
+
+            Point point = (Point) o;
+
+            if (getSection() != point.getSection()) return false;
+            return getMultiplier() == point.getMultiplier();
+        }
+
+        @Override
+        public int hashCode() {
+            int result = getSection();
+            result = 31 * result + getMultiplier();
+            return result;
+        }
+    }
+
+    private class Game {
+        private Point first;
+        private Point second;
+        private Point third;
+
+        public Game(Point first, Point second, Point third) {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+
+        public Point getFirst() {
+            return first;
+        }
+
+        public Point getSecond() {
+            return second;
+        }
+
+        public Point getThird() {
+            return third;
+        }
+
+        public boolean isCheckout() {
+            return third.getMultiplier() == 2
+                    || (third.getSection() == 0 && second.getMultiplier() == 2)
+                    || (third.getSection() == 0 && second.getSection() == 0 && first.getMultiplier() == 2);
+        }
+
+        public int getScore() {
+            return first.getValue() + second.getValue() + third.getValue();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Game)) return false;
+
+            Game game = (Game) o;
+
+            if (!getFirst().equals(game.getFirst())) return false;
+            if (!getSecond().equals(game.getSecond())) return false;
+            return getThird().equals(game.getThird());
+        }
+
+        @Override
+        public int hashCode() {
+            int result = getFirst().hashCode();
+            result = 31 * result + getSecond().hashCode();
+            result = 31 * result + getThird().hashCode();
+            return result;
+        }
     }
 }
