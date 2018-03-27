6908ba99875e51cd5383388eb662af8e91a1a2dc
sageeprice <sageeprice@gmail.com>
2018-03-26 21:43:54 -0400

    Changed URL link in javadoc for 622

--- src/problems/impl/Euler622.java
+++ src/problems/impl/Euler622.java
@@ -8,8 +8,8 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Math behind this is fairly well-known, see
- * {@link <@a href="https://en.wikipedia.org/wiki/Out_shuffle">Wikipedia</a>}.
+ * Math behind this is fairly well-known
+ * @see <@a href="https://en.wikipedia.org/wiki/Out_shuffle">Wikipedia</a>.
  *
  * Answer: 3010983666182123972
  */
